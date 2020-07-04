3127cdb219e0974d6e6d603200254dfd376140f3
Ozmar Mendoza <ozmarmen@gmail.com>
2020-07-03 20:12:51 -0500

    Create writable store productDetailsVisibility

--- src/utils/stores/index.js
+++ src/utils/stores/index.js
@@ -3,4 +3,5 @@ import { writable } from 'svelte/store';
 
 export { NavSublinks };
 export const activeProduct = writable({});
+export const productDetailsVisibility = writable(false);
 export const mainMenuVisibility = writable(false);
