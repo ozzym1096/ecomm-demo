095c87a5ac8dcefa535e0e77fa84a31224efff95
Ozmar Mendoza <ozmarmen@gmail.com>
2020-08-12 23:11:47 -0500

    Rename product stores

--- src/utils/stores/index.js
+++ src/utils/stores/index.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
 export { mainMenuPages } from './mainMenuPages';
-export const activeProduct = writable({});
-export const productDetailsVisibility = writable(false);
+export const currDetailedProduct = writable({});
+export const detailedProductVisibility = writable(false);
 export const mainMenuVisibility = writable(false);
