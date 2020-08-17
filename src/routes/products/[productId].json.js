0256f39ab7c8769007612e9e07bd926d277cc166
Ozmar Mendoza <ozmarmen@gmail.com>
2020-08-17 09:16:05 -0500

    Create route for indivual product page

---
+++ src/routes/products/[productId].json.js
@@ -0,0 +1,12 @@
+import db from '../../utils/db';
+
+export async function get(req, res, next) {
+	try {
+		const { rows } = await db.query('select * from products where id = $1', [req.params.productId]);
+		res.setHeader('Content-Type', 'application/json');
+		res.json(rows[0]);
+	}
+	catch (err) {
+		next(err);
+	}
+}
---
+++ src/routes/products/[productId].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function preload(page) {
+		const res = await this.fetch(`products/${page.params.productId}.json`);
+		const data = await res.json();
+		return { product: data };
+	}
+</script>
+
+<script>
+	export let product;
+</script>
+
+<h2>{product.name}</h2>
+<img src="{product.image}" alt="" />
