9301da476f4af81bf28b9ca99513c6f05b4cdc9c
Ozmar Mendoza <ozmarmen@gmail.com>
2020-08-19 15:07:28 -0500

    Test route

---
+++ src/routes/category-1/link-1.svelte
@@ -0,0 +1,18 @@
+<div>
+	<span>This is link 1 in category 1</span>
+</div>
+
+<style>
+	div {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	span {
+		font-size: 50px;
+		margin-top: 2em;
+	}
+</style>
