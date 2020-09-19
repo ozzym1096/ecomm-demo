c697da87136b4fa6db57f27acd1c44908d354414
Ozmar Mendoza <ozmarmen@gmail.com>
2020-09-19 12:58:41 -0500

    Navbar scroll animation for portrait orientation

--- src/components/MainNavList.svelte
+++ src/components/MainNavList.svelte
@@ -41,6 +41,34 @@
 		flex-direction: row;
 		overflow-x: auto;
 		white-space: nowrap;
+		margin-top: 0;
+		margin-bottom: 0;
+	}
+
+	.nav-list-item {
+		display: inline-block;
+		padding-left: 1em;
+		padding-right: 1em;
+		padding-top: 0.7em;
+		padding-bottom: 0.7em;
+		margin-left: 0.5em;
+		margin-right: 0.5em;
+	}
+
+	@keyframes navListScroll {
+		0% {
+			margin-left: 0.5em;
+		}
+		50% {
+			margin-left: -30px;
+		}
+		100% {
+			margin-left: 0.5em;
+		}
+	}
+
+	.nav-list-item > a {
+		text-decoration: underline;
 	}
 
 	@media (orientation: landscape) {
@@ -49,15 +77,9 @@
 		}
 	}
 
-	.nav-list-item {
-		display: inline-block;
-		padding-left: 1em;
-		padding-right: 1em;
-		margin-left: 0.5em;
-		margin-right: 0.5em;
-	}
-
-	.nav-list-item > a {
-		text-decoration: underline;
+	@media (orientation: portrait) {
+		.nav-list-item:first-of-type {
+			animation: 1s ease-out 0.5s navListScroll;
+		}
 	}
 </style>
