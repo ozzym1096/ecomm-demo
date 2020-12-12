25d07b46026a0f9d6e758beedb35b7b6219b9610
Ozmar Mendoza <ozmarmen@gmail.com>
2020-12-12 12:18:50 -0600

    Initializa svelte-cloudinary in [productId].svelte

--- src/routes/products/[productId].svelte
+++ src/routes/products/[productId].svelte
@@ -1,15 +1,19 @@
 <script context="module">
-	export async function preload(page) {
+	export async function preload(page, session) {
+		const { CLOUDINARY_NAME } = session;
 		const res = await this.fetch(`products/${page.params.productId}.json`);
 		const data = await res.json();
-		return { product: data };
+		return { product: data, clName: CLOUDINARY_NAME };
 	}
 </script>
 
 <script>
 	export let product;
+	export let clName;
 
-	import { image } from "svelte-cloudinary";
+	import { initialize, image } from "svelte-cloudinary";
+
+	initialize({ cloud_name: clName });
 </script>
 
 <h2>{product.name}</h2>
@@ -17,3 +21,10 @@
 	use:image="{{ src: `\/ecomm-demo\/${product.image}`, bind: true, lazy: true, options: { crop: 'lfill', gravity: 'auto' } }}"
 	alt="{`Product photo of ${product.name}`}"
 />
+l
+
+<style>
+	img {
+		width: 65vw;
+	}
+</style>
