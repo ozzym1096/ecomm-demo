5e4215132d1a2b15569a4319b0780c692386ff58
Ozmar Mendoza <ozmarmen@gmail.com>
2020-12-16 18:36:02 -0600

    Wrap cloudinary image with wrapper

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -17,11 +17,13 @@
 
 <li class="product-card">
 	<a class="product-card-link" href="/products/{product.id}">
-		<img
-			use:image="{{ src: `\/ecomm-demo\/${product.image}`, bind: this, lazy: true, options: { crop: 'lfill', gravity: 'auto' } }}"
-			class="product-card-image card-content"
-			alt="{`Product photo of ${product.name}`}"
-		/>
+		<div class="product-card-image card-content">
+			<img
+				use:image="{{ src: `\/ecomm-demo\/${product.image}`, bind: '.product-card-image', lazy: true, options: { crop: 'lfill', gravity: 'auto' } }}"
+				style="width: 100%; height: 100%;"
+				alt="{`Product photo of ${product.name}`}"
+			/>
+		</div>
 		<div class="product-card-info card-info card-content">
 			<h2 class="product-card-info-name">{product.name}</h2>
 			<p>${product.price}</p>
@@ -47,8 +49,6 @@
 		height: 125px;
 		margin-top: 1rem;
 		margin-bottom: 1rem;
-		object-position: center;
-		object-fit: cover;
 	}
 
 	.product-card-info {
