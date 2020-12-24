cabb425c50d6781c22c02a5041eaf586ad13729b
Ozmar Mendoza <ozmarmen@gmail.com>
2020-12-24 13:53:09 -0600

    Only fetch used product data on All Products page

--- src/routes/products/index.json.js
+++ src/routes/products/index.json.js
@@ -2,7 +2,7 @@ import db from "../../utils/db";
 
 export async function get(_req, res, next) {
 	try {
-		const { rows } = await db.query("select * from products");
+		const { rows } = await db.query("select name, price, image, id from products");
 		res.writeHead(200, { "Content-Type": "application/json" });
 		res.end(JSON.stringify(rows));
 	}
