5bfc3a0a9298b0c9e28917d290489fb8bc775eee
Ozmar Mendoza <ozmarmen@gmail.com>
2021-03-23 18:56:16 -0500

    Update db object

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -3,9 +3,9 @@ import { Pool } from "pg";
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	// Comment out ssl when testing locally
-	ssl: {
-		rejectUnauthorized: false
-	}
+	// ssl: {
+	// 	rejectUnauthorized: false
+	// }
 });
 
 pool.on("error", (err) => {
@@ -14,8 +14,33 @@ pool.on("error", (err) => {
 });
 
 const db = {
-	query: function (text, params) {
-		return pool.query(text, params);
+	query: async (text, params) => {
+		const { rows: res } = await pool.query(text, params);
+		return res;
+	},
+	getClient: async () => {
+		const client = await pool.connect()
+		const query = client.query
+		const release = client.release
+		// set a timeout of 5 seconds, after which we will log this client's last query
+		const timeout = setTimeout(() => {
+			console.error('A client has been checked out for more than 5 seconds!')
+			console.error(`The last executed query on this client was: ${client.lastQuery}`)
+		}, 5000)
+		// monkey patch the query method to keep track of the last query executed
+		client.query = (...args) => {
+			client.lastQuery = args
+			return query.apply(client, args)
+		}
+		client.release = () => {
+			// clear our timeout
+			clearTimeout(timeout)
+			// set the methods back to their old un-monkey-patched version
+			client.query = query
+			client.release = release
+			return release.apply(client)
+		}
+		return client
 	}
 };
 
