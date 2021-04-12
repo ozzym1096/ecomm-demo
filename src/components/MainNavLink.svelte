0ff645602f04a1cf67822fdf203fae34b987499f
Ozmar Mendoza <ozmarmen@gmail.com>
2021-04-11 22:39:31 -0500

    Apply Tailwind classes to Nav Bar component

---
+++ src/components/MainNavLink.svelte
@@ -0,0 +1,36 @@
+<script context="module">
+	import { stores } from "@sapper/app";
+</script>
+
+<script>
+	export let href;
+
+	const { page } = stores();
+</script>
+
+<li>
+	<a
+		class="font-bold"
+		href="{href}"
+		aria-current="{$page.path === href ? 'page' : undefined}"
+	>
+		<div
+			class="nav-link min-w-min w-12 h-12 flex flex-col justify-center items-center"
+		>
+			<slot />
+		</div>
+	</a>
+</li>
+
+<style global lang="postcss">
+	@layer base {
+		[aria-current] > .nav-link {
+			@apply text-blue-800;
+		}
+
+		[aria-current] > .nav-link::before {
+			content: "\25BC";
+			@apply text-xs;
+		}
+	}
+</style>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -8,7 +8,7 @@
 <script>
 	export let cloud_name;
 
-	import MainNavList from "../components/MainNavList.svelte";
+	import MainNav from "../components/MainNav.svelte";
 	import { initialize } from "svelte-cloudinary";
 
 	initialize({
@@ -16,101 +16,13 @@
 	});
 </script>
 
-<header id="main-header" class="l-wrapper">
-	<div class="l-display-contents">
-		<a class="main-header-logo" href="/">Craaaiiig's</a>
-		<nav id="nav-main" aria-label="Website categories">
-			<MainNavList />
-		</nav>
-	</div>
-</header>
-<main>
-	<div class="l-wrapper">
-		<slot />
-	</div>
+<MainNav />
+<main class="bg-gray-100 px-7 pt-3 pb-24 sm:pb-10 sm:px-12 md:px-16 lg:px-20">
+	<slot />
 </main>
 
 <style global lang="postcss">
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-
-	#main-header {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		margin-top: 5vh;
-		margin-bottom: 5vh;
-	}
-
-	.main-header-logo {
-		font-size: var(--font-size-large);
-		font-family: "Rock Salt", cursive;
-		font-weight: var(--font-weight-bolder);
-		color: var(--color-orange);
-	}
-
-	#nav-main {
-		position: fixed;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		z-index: 100;
-		background-color: var(--color-orange-light);
-		box-shadow: 0px 7px 11px 0px #5e5e5e;
-	}
-
-	main {
-		margin-bottom: 12vh;
-	}
-
-	@media (orientation: landscape) and (max-height: 500px) {
-		#main-header,
-		main {
-			width: 85%;
-		}
-
-		#main-header {
-			margin-left: initial;
-			margin-top: 9vh;
-			margin-bottom: 7vh;
-		}
-
-		#nav-main {
-			width: 15%;
-			height: 100%;
-			bottom: initial;
-			left: initial;
-			top: 0;
-			box-shadow: 7px 0px 11px 0px #5e5e5e;
-		}
-
-		main {
-			margin-bottom: 3vh;
-		}
-	}
-
-	@media (min-width: 671px) and (min-height: 501px) {
-		header,
-		main {
-			--padding: 5vw;
-		}
-
-		#main-header {
-			justify-content: space-between;
-			margin-top: 4vh;
-			margin-bottom: 3vh;
-		}
-
-		#nav-main {
-			display: contents;
-			box-shadow: initial;
-			background-color: initial;
-		}
-
-		main {
-			margin-bottom: 5vh;
-		}
-	}
 </style>
